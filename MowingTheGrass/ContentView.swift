fa8230d577f8fc5ada6a810b188c3d9368e7964f
Gregg Jaskiewicz <gryzman@gmail.com>
2025-06-12 15:41:45 +0100

    Initial Commit

---
+++ MowingTheGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MowingTheGrass
+//
+//  Created by Gregg Jaskiewicz on 12/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MowingTheGrass/MowingTheGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MowingTheGrassApp.swift
+//  MowingTheGrass
+//
+//  Created by Gregg Jaskiewicz on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MowingTheGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
